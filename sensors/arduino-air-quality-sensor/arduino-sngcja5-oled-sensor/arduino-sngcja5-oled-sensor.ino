80a85ad9c5fb8ff3b5b4aee710003a33a488e1da
Scott Hraban <scotthraban@gmail.com>
2020-10-13 20:56:13 -0700

    3 lines output, no button

--- sensors/arduino-air-quality-sensor/arduino-sngcja5-oled-sensor/arduino-sngcja5-oled-sensor.ino
+++ sensors/arduino-air-quality-sensor/arduino-sngcja5-oled-sensor/arduino-sngcja5-oled-sensor.ino
@@ -4,53 +4,79 @@
 #include <Adafruit_SSD1306.h>
 #include "SparkFun_Particle_Sensor_SN-GCJA5_Arduino_Library.h"
 
-#define CURRENT_INTERVAL 30000
-
-#define SAMPLE_COUNT 96
-#define SAMPLE_INTERVAL 900000
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
 #define DISPLAY_PERIOD 5000
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 SFE_PARTICLE_SENSOR airSensor;
 
-int current = -1;
-long currentTaken = 0;
-
-int samples[SAMPLE_COUNT];
 long sampleTaken = 0;
-int currentSampleIndex = SAMPLE_COUNT - 1;
+
+int samplePm25[60];
+int samplePm10[60];
+int sampleIndex = 0;
+
+int averagePm25[30];
+int averagePm10[30];
+int averageIndex;
 
 long displayStarted =  0;
 bool displayStopped = true; 
 
 void setup() {
   
-  pinMode(LED_BUILTIN, OUTPUT);
-
   Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  bool airSensorStatus = airSensor.begin();
+  airSensor.begin();
   
-  for (int i = 0; i < SAMPLE_COUNT; i++) {
-    samples[i] = -1;
+  for (int i = 0; i < 60; i++) {
+    samplePm25[i] = -1;
+    samplePm10[i] = -1;
+  }
+  for (int i = 0; i < 30; i++) {
+    averagePm25[i] = -1;
+    averagePm10[i] = -1;
   }
 }
 
 void loop() {
+  if (millis() > (sampleTaken + 1000)) {
+    samplePm25[sampleIndex] = getPm25Aqi();
+    samplePm10[sampleIndex] = getPm10Aqi();
 
-  if (millis() > (currentTaken + CURRENT_INTERVAL)) {
-    current = getPm25Aqi();
-    samples[currentSampleIndex] = current;
+    sampleIndex = (sampleIndex +1) % 60;
 
-    if (millis() > (sampleTaken + SAMPLE_INTERVAL)) {
-      currentSampleIndex = (currentSampleIndex + 1) % SAMPLE_COUNT;
-      sampleTaken = millis();
+    if (sampleIndex == 0) {
+      averagePm25[averageIndex] = arrayAverage(samplePm25, 60);
+      averagePm10[averageIndex] = arrayAverage(samplePm10, 60);
+
+      averageIndex = (averageIndex + 1) % 30;
     }
+
+    sampleTaken = millis();
   }
 
   doDisplay();
+}
+
+int arrayAverage(int arr[], int len) {
+  int sum = 0;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    if (arr[i] != -1) {
+      sum += arr[i];
+      count++;
+    }
+  }
+  
+  if (count > 0) {
+    return sum / count;
+  } else {
+    return -1;
+  }
 }
 
 void doDisplay() {
@@ -68,12 +94,21 @@ void doDisplay() {
 void startDisplay() {
 
   display.clearDisplay();
-  display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(4, 24);
   display.cp437(true);
+  display.setTextSize(2);
+  display.setCursor(0, 0);
 
-  display.print(current);
+  // We get about ten characters per line
+  
+  printPadded(arrayAverage(samplePm25, 60));
+  display.println(" 2.5/1");
+
+  printPadded(arrayAverage(samplePm10, 60));
+  display.println("  10/1");
+
+  printPadded(arrayAverage(averagePm25, 30));
+  display.println(" 2.5/30");
 
   display.display();
 
@@ -81,6 +116,16 @@ void startDisplay() {
 
   displayStarted = millis();
   displayStopped = false;
+}
+
+void printPadded(int aqi) {
+  if (aqi < 10) {
+    display.print("  ");
+  } else if (aqi < 100) {
+    display.print(" ");
+  }
+
+  display.print(aqi);
 }
 
 void stopDisplay() {
@@ -106,7 +151,53 @@ void stopDisplay() {
 //}
 
 int getPm25Aqi() {
-  int pm25 = airSensor.getPM2_5();
-  currentTaken = millis();
-  return pm25;
+  return calculatePm25Aqi(airSensor.getPM2_5());
+}
+
+int calculatePm25Aqi(float pm2_5) {
+  // https://forum.airnowtech.org/t/the-aqi-equation/169
+
+  float truncated = trunc(pm2_5 * 10.0) / 10.0;
+
+  if (truncated >= 250.5) {
+    return calculateAqi(truncated, 250.5, 500.4, 301, 500);
+  } else if (truncated >= 150.5) {
+    return calculateAqi(truncated, 150.5, 250.4, 201, 300);
+  } else if (truncated >= 55.5) {
+    return calculateAqi(truncated, 55.5, 150.4, 151, 200);
+  } else if (truncated >= 35.5) {
+    return calculateAqi(truncated, 35.5, 55.4, 101, 150);
+  } else if (truncated >= 12.1) {
+    return calculateAqi(truncated, 12.1, 35.4, 51, 100);
+  } else {
+    return calculateAqi(truncated, 0.0, 12.0, 0, 50);
+  }
+}
+
+int getPm10Aqi() {
+  return calculatePm10Aqi(airSensor.getPM10());
+}
+
+int calculatePm10Aqi(float pm10) {
+  // https://forum.airnowtech.org/t/the-aqi-equation/169
+
+  float truncated = trunc(pm10);
+
+  if (truncated >= 425.0) {
+    return calculateAqi(truncated, 425.0, 604.0, 301, 500);
+  } else if (truncated >= 355.0) {
+    return calculateAqi(truncated, 355.0, 424.0, 201, 300);
+  } else if (truncated >= 255.0) {
+    return calculateAqi(truncated, 255.0, 354.0, 151, 200);
+  } else if (truncated >= 155.0) {
+    return calculateAqi(truncated, 155.0, 254.0, 101, 150);
+  } else if (truncated >= 55.0) {
+    return calculateAqi(truncated, 55.0, 154.0, 51, 100);
+  } else {
+    return calculateAqi(truncated, 0.0, 54.0, 0, 50);
+  }
+}
+
+int calculateAqi(float concI, float concL, float concH, int aqiL, int aqiH) {
+  return (((aqiH - aqiL) / (concH - concL)) * (concI - concL)) + aqiL;
 }
