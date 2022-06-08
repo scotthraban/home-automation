faa3f16f4764208289c5a8b9b110a3d7c7263a00
Scott Hraban <scotthraban@gmail.com>
2022-06-07 20:31:36 -0700

    update tts service to python 3 and add .service file

--- tts/tts-server.py
+++ tts/tts-server.py
@@ -1,26 +1,27 @@
+import os
 import time
 import json
-import commands
+import subprocess
 import threading
-import BaseHTTPServer
+import http.server
 
 HOST_NAME = ''
 PORT_NUMBER = 8080
 
 def play(msg, volume, locale):
-    commands.getoutput("pico2wave -l \"" + locale + "\" -w pico2wave.wav \"" + msg + "\"")
-    commands.getoutput("omxplayer --vol -750 chime.wav")
-    commands.getoutput("omxplayer --vol " + str(volume) + " pico2wave.wav")
+    subprocess.run(["pico2wave", "-l", locale, "-w", "pico2wave.wav", msg])
+    subprocess.run(["paplay", "--volume=32768", "chime.wav"])
+    subprocess.run(["paplay", "--volume=" + str(volume), "pico2wave.wav"])
 
 
-class TtsHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class TtsHandler(http.server.BaseHTTPRequestHandler):
 
     def do_POST(s):
 
-        body = json.loads(s.rfile.read(int(s.headers.getheader("content-length", 0))))
+        body = json.loads(s.rfile.read(int(s.headers.get("content-length", 0))))
         msg = body.get("msg")
         locale = body.get("locale", "en-US")
-        volume = body.get("volume", -500)
+        volume = body.get("volume", 50000)
 
         threading.Thread(target = play, args = (msg, volume, locale)).start()
 
@@ -28,14 +29,13 @@ class TtsHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_class = BaseHTTPServer.HTTPServer
+    server_class = http.server.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), TtsHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    print(time.asctime(), "Server Starts in %s - %s:%s" % (os.getcwd(), HOST_NAME, PORT_NUMBER))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
-
+    print(time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER))
 
