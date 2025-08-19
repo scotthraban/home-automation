89ef0cdd111d657fba4506439309bd8aad0cfd3c
Scott Hraban <scotthraban@gmail.com>
2025-08-19 16:34:15 -0700

    fix build script to work with podman

--- telegram-bot/build.sh
+++ telegram-bot/build.sh
@@ -1,6 +1,11 @@
-docker buildx build \
-  --platform linux/arm64/v8,linux/amd64 \
-  --push \
-  --tag docker-registry.hraban.com/telegram-bot:latest \
-  --tag docker-registry.hraban.com/telegram-bot:4.1 \
-  .
+#!/bin/bash
+
+podman manifest rm telegram-bot:latest
+podman rmi telegram-bot:latest
+podman build --platform linux/amd64,linux/arm64 --manifest telegram-bot:latest .
+podman manifest push telegram-bot:latest docker-registry.hraban.com/telegram-bot:latest
+
+podman manifest rm telegram-bot:5.1
+podman rmi telegram-bot:5.1
+podman build --platform linux/amd64,linux/arm64 --manifest telegram-bot:5.1 .
+podman manifest push telegram-bot:5.1 docker-registry.hraban.com/telegram-bot:5.1
